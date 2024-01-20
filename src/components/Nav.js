69251ca1bc2e54247c323f26a0b170d1e20a24c5
sagitace <aaronlabini27.official@gmail.com>
2024-01-20 12:28:31 +0800

    Navbar - Almost done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   FaGithub,
   FaFacebookSquare,
@@ -8,31 +8,103 @@ import {
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Nav = () => {
+  // This is to toggle the nav (hamburger button) the nav is closed by default
   const [nav, setNav] = useState(false);
 
+  // This is to set the active link the active link is set to Home by default
+  const [activeLink, setActiveLink] = useState(
+    localStorage.getItem("activeLink") || "Home"
+  );
+
+  // This is to retain the active link when the page is refreshed
+  useEffect(() => {
+    localStorage.setItem("activeLink", activeLink);
+  }, [activeLink]);
+
+  // This is to close the nav (hamburger button) when a link is clicked
   const handleNav = () => {
     setNav(!nav);
   };
 
+  // This is to change the active link when a link is clicked
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
-    <nav className="flex justify-between items-center h-20 max-w-[1240px] mx-auto px-4 text-white">
+    <nav className="flex justify-between items-center h-18 max-w-[1240px] mx-auto px-4 text-white">
       {" "}
-      <h1 className="text-xl font-bold"> Aaron. </h1>{" "}
+      <h1 className="text-xl font-bold">
+        <a href="#About" onClick={() => handleLinkClick("About")}>
+          {" "}
+          Aaron.{" "}
+        </a>{" "}
+      </h1>{" "}
       <ul className="space-x-4 hidden md:flex">
         <li className="p-4">
-          <a href="#Home"> Home </a>{" "}
+          <a
+            href="#Home"
+            onClick={() => handleLinkClick("Home")}
+            className={`tracking-widest relative group ${
+              activeLink === "Home" ? "text-red-400" : "text-white/90"
+            }`}
+          >
+            {" "}
+            Home{" "}
+            <div className="absolute w-full h-0.5 bg-red-400 scale-x-0 group-hover:scale-x-100 transition-transform" />
+          </a>{" "}
         </li>{" "}
         <li className="p-4">
-          <a href="#About"> About </a>{" "}
+          <a
+            href="#About"
+            onClick={() => handleLinkClick("About")}
+            className={`tracking-widest relative group ${
+              activeLink === "About" ? "text-red-400" : "text-white/90"
+            }`}
+          >
+            {" "}
+            About{" "}
+            <div className="absolute w-full h-0.5 bg-red-400 scale-x-0 group-hover:scale-x-100 transition-transform" />
+          </a>{" "}
         </li>{" "}
         <li className="p-4">
-          <a href="#Skills"> Skills </a>{" "}
+          <a
+            href="#Skills"
+            onClick={() => handleLinkClick("Skills")}
+            className={`tracking-widest relative group ${
+              activeLink === "Skills" ? "text-red-400" : "text-white/90"
+            }`}
+          >
+            {" "}
+            Skills{" "}
+            <div className="absolute w-full h-0.5 bg-red-400 scale-x-0 group-hover:scale-x-100 transition-transform" />
+          </a>{" "}
         </li>{" "}
         <li className="p-4">
-          <a href="#Projects"> Projects </a>{" "}
+          <a
+            href="#Projects"
+            onClick={() => handleLinkClick("Projects")}
+            className={`tracking-widest relative group ${
+              activeLink === "Projects" ? "text-red-400" : "text-white/90"
+            }`}
+          >
+            {" "}
+            Projects{" "}
+            <div className="absolute w-full h-0.5 bg-red-400 scale-x-0 group-hover:scale-x-100 transition-transform" />
+          </a>{" "}
         </li>{" "}
         <li className="p-4">
-          <a href="#Projects"> Contact </a>{" "}
+          <a
+            href="#Contact"
+            onClick={() => handleLinkClick("Contact")}
+            className={`tracking-widest relative group ${
+              activeLink === "Contact" ? "text-red-400" : "text-white/90"
+            }`}
+          >
+            {" "}
+            Contact{" "}
+            <div className="absolute w-full h-0.5 bg-red-400 scale-x-0 group-hover:scale-x-100 transition-transform" />
+          </a>{" "}
         </li>{" "}
         <li className="p-4">
           <a
@@ -69,7 +141,7 @@ const Nav = () => {
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#2b2d42] ease-in-out duration-500"
+            ? "fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#2b2d42] ease-in-out duration-500 block md:hidden"
             : "fixed left-[-100%]"
         }
       >
