7759039fa488c788f341b7d671fef918fcc0edad
sagitace <aaronlabini27.official@gmail.com>
2024-02-04 21:43:52 +0800

    resume update and go to top button

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import RingLoader from "react-spinners/RingLoader";
+import { FaArrowUp } from "react-icons/fa";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -17,6 +19,23 @@ export default function App() {
       setLoading(false);
     }, 2000);
   }, []);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisibility);
 
   return (
     <div className="App">
@@ -56,6 +75,14 @@ export default function App() {
           <div>
             <Footer />
           </div>{" "}
+          <button
+            className={`fixed bottom-0 right-0 p-4 m-4 text-white bg-red-500 rounded-full transition-opacity duration-500 ${
+              isVisible ? "opacity-100" : "opacity-0"
+            }`}
+            onClick={scrollToTop}
+          >
+            <FaArrowUp />
+          </button>
         </div>
       )}
     </div>
